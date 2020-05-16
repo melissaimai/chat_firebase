a8382163d3113eb9a834a17a6488cac75965c9bc
Juliana <jtacacima@gmail.com>
2020-05-15 22:41:42 -0300

    Initial commit

---
+++ app/src/main/java/com/example/fatec_ipi_noite_chat_com_firebase/DateHelper.java
@@ -0,0 +1,12 @@
+package com.example.fatec_ipi_noite_chat_com_firebase;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+class DateHelper {
+    private static SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss");
+
+    public static String format (Date date){
+        return sdf.format(date);
+    }
+}
